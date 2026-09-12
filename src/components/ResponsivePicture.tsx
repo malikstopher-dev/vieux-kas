4f0bffe1f3f015311749b6270166d9995779dab4
Stopher Malik <malikstopher@gmail.com>
2026-09-12 10:28:07 +0200

    Fix PremiumHero runtime and mobile drawer

--- src/components/ResponsivePicture.tsx
+++ src/components/ResponsivePicture.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 
-import {getImageProps} from "next/image";
-
 type Props = {
   desktopSrc: string;
   mobileSrc: string;
@@ -11,18 +9,18 @@ type Props = {
 };
 
 export function ResponsivePicture({desktopSrc, mobileSrc, alt, className = "", priority = false}: Props) {
-  const {props: mobileProps} = getImageProps({src: mobileSrc, alt, width: 941, height: 1672, sizes: "100vw", quality: 88});
-  const {props: desktopProps} = getImageProps({src: desktopSrc, alt, width: 1672, height: 941, sizes: "100vw", quality: 88});
-
   return <>
     {priority && <>
-      <link rel="preload" as="image" imageSrcSet={mobileProps.srcSet} imageSizes={mobileProps.sizes} media="(max-width: 767px)" />
-      <link rel="preload" as="image" imageSrcSet={desktopProps.srcSet} imageSizes={desktopProps.sizes} media="(min-width: 768px)" />
+      <link rel="preload" as="image" href={mobileSrc} media="(max-width: 767px)" />
+      <link rel="preload" as="image" href={desktopSrc} media="(min-width: 768px)" />
     </>}
     <picture className={className}>
-      <source media="(max-width: 767px)" srcSet={mobileProps.srcSet} sizes={mobileProps.sizes} />
+      <source media="(max-width: 767px)" srcSet={mobileSrc} />
       <img
-        {...desktopProps}
+        src={desktopSrc}
+        width={1672}
+        height={941}
+        alt={alt}
         loading={priority ? "eager" : "lazy"}
         fetchPriority={priority ? "high" : "auto"}
         decoding="async"
